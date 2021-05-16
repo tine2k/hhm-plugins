4c6046a8552dd87bcaaaf0c43d371c083865c5cf
Martin Maier-Moessner <martin.maier.moessner@gmail.com>
2021-05-16 20:42:51 +0200

    add balance plugin with tests

--- src/tine2k/balance.js
+++ src/tine2k/balance.js
@@ -17,17 +17,55 @@ room.pluginSpec = {
 
 room.onCommand_balance = () => {
     const config = room.getConfig();
-    getData(config.url).then(body => {
-        room.sendAnnouncement('PlayerList: ' + room.getPlayerList());
+    return getData(config.url).then(body => {
+        const minRating = Math.min(...body.map(p => p.rating));
+        const lowestRating = minRating === Infinity ? 0.0 : minRating;
         const players = room.getPlayerList()
             .filter(p => p.name !== 'host')
             .map(p => ({
                 id: p.id,
-                rating: body.filter(r => r.name === p.name).map(r => r.rating)[0]
+                name: p.name,
+                rating: body.filter(r => r.name === p.name).map(r => r.rating)[0] || lowestRating
             }));
-        room.sendAnnouncement('Players: ' + JSON.stringify(players));
+        const balancedTeams = balanceTeams(players, Math.round(players.length / 2), [], 0, 0);
+        // console.log(balancedTeams);
+
+        balancedTeams.team1.forEach(p => room.setPlayerTeam(p.id, 1));
+        balancedTeams.team2.forEach(p => room.setPlayerTeam(p.id, 2));
+
+        room.sendAnnouncement('Player ratings: ' + players.map(p => `${p.name}: ${p.rating.toFixed(2)}`).reduce((a,b) => `${a}, ${b}`));
+        room.sendAnnouncement(`Teams balanced: ${balancedTeams.r1.toFixed(2)} vs ${balancedTeams.r2.toFixed(2)}`);
     });
 };
+
+function balanceTeams(players, teamSize, team, posIndex, playerIndex, bestResult) {
+    let currentResult = bestResult;
+    for (let i = playerIndex; i < players.length; i++) {
+        team[posIndex] = players[i];
+        if (posIndex < teamSize - 1) {
+            currentResult = balanceTeams(players, teamSize, team.concat(), posIndex + 1, i + 1, currentResult) || currentResult;
+        } else {
+            const otherTeam = players.filter(p => !team.includes(p));
+            const r1 = rateTeam(team);
+            const r2 = rateTeam(otherTeam);
+            if (!bestResult || Math.abs(r1 - r2) < Math.abs(bestResult.r1 - bestResult.r2)) {
+                return {
+                    r1: r1,
+                    r2: r2,
+                    team1: team.concat(),
+                    team2: otherTeam
+                }
+            } else {
+                return bestResult;
+            }
+        }
+    }
+    return currentResult;
+}
+
+function rateTeam(team) {
+    return team.map(i => i.rating).reduce((a,b) => a + b);
+}
 
 async function getData(url) {
     try {
@@ -39,7 +77,9 @@ async function getData(url) {
         });
 
         if (!response.ok) {
-            logError(`Error getting ratings, bad response from API: ${response.status} - ${response.statusText}`);
+            let errorText = `Error getting ratings, bad response from API: ${response.status} - ${response.statusText}`;
+            logError(errorText);
+            return Promise.reject(errorText);
         } else {
             return await response.json();
         }
---
+++ src/tine2k/balance.spec.js
@@ -0,0 +1,125 @@
+const roomSpy = jasmine.createSpyObj(['getConfig', 'onCommand_balance', 'sendAnnouncement', 'getPlayerList', 'setPlayerTeam']);
+global.HBInit = () => {
+    return roomSpy;
+};
+require('./balance');
+
+describe('plugin', () => {
+    beforeEach(() => {
+        roomSpy.sendAnnouncement.and.callFake(a => console.log('ANN: ' + a));
+        roomSpy.getConfig.and.returnValue({
+            url: 'myUrl'
+        });
+    });
+
+    it('should have to correct name', () => {
+        expect(roomSpy.pluginSpec.name).toEqual('tine2k/balance');
+    });
+
+    it('should send announcement on 500', () => {
+        global.fetch = () => {
+            return Promise.resolve({
+                ok: false,
+                status: 500,
+                statusText: 'Internal Server Error'
+            });
+        };
+
+        roomSpy.getPlayerList.and.returnValue([]);
+        let errorText = 'Error getting ratings, bad response from API: 500 - Internal Server Error';
+        roomSpy.onCommand_balance().catch(e => {
+            expect(roomSpy.sendAnnouncement).toHaveBeenCalledWith(errorText, null, 16711680, 'bold');
+            expect(e).toEqual(errorText);
+        });
+    });
+
+    it('should balance team of known players', () => {
+        const ratings = [
+            {name: 'A', rating: 0.1},
+            {name: 'B', rating: 0.4},
+            {name: 'C', rating: 0.6},
+            {name: 'D', rating: 0.7},
+            {name: 'E', rating: 0.2}
+        ];
+        const playerList = [
+            {id: 0, name: 'host'},
+            {id: 1, name: 'A'},
+            {id: 2, name: 'B'},
+            {id: 3, name: 'C'},
+            {id: 4, name: 'D'},
+            {id: 5, name: 'E'},
+        ];
+        global.fetch = () => {
+            return Promise.resolve({
+                ok: true,
+                status: 200,
+                json: () => Promise.resolve(ratings)
+            });
+        };
+
+        roomSpy.getPlayerList.and.returnValue(playerList);
+        roomSpy.onCommand_balance().then(() => {
+            expect(roomSpy.setPlayerTeam).toHaveBeenCalledWith(1, 1);
+            expect(roomSpy.setPlayerTeam).toHaveBeenCalledWith(2, 2);
+            expect(roomSpy.setPlayerTeam).toHaveBeenCalledWith(3, 2);
+            expect(roomSpy.setPlayerTeam).toHaveBeenCalledWith(4, 1);
+            expect(roomSpy.setPlayerTeam).toHaveBeenCalledWith(5, 1);
+            expect(roomSpy.sendAnnouncement).toHaveBeenCalledWith('Player ratings: A: 0.10, B: 0.40, C: 0.60, D: 0.70, E: 0.20');
+            expect(roomSpy.sendAnnouncement).toHaveBeenCalledWith('Teams balanced: 1.00 vs 1.00');
+        });
+    });
+
+    it('should balance team of some unknown players', () => {
+        const ratings = [
+            {name: 'A', rating: 0.1},
+            {name: 'B', rating: 0.2}
+        ];
+        const playerList = [
+            {id: 0, name: 'host'},
+            {id: 1, name: 'A'},
+            {id: 2, name: 'B'},
+            {id: 3, name: 'C'},
+            {id: 4, name: 'D'},
+        ];
+        global.fetch = () => {
+            return Promise.resolve({
+                ok: true,
+                status: 200,
+                json: () => Promise.resolve(ratings)
+            });
+        };
+
+        roomSpy.getPlayerList.and.returnValue(playerList);
+        roomSpy.onCommand_balance().then(() => {
+            expect(roomSpy.setPlayerTeam).toHaveBeenCalledWith(1, 1);
+            expect(roomSpy.setPlayerTeam).toHaveBeenCalledWith(2, 1);
+            expect(roomSpy.setPlayerTeam).toHaveBeenCalledWith(3, 2);
+            expect(roomSpy.setPlayerTeam).toHaveBeenCalledWith(4, 2);
+            expect(roomSpy.sendAnnouncement).toHaveBeenCalledWith('Player ratings: A: 0.10, B: 0.20, C: 0.10, D: 0.10');
+            expect(roomSpy.sendAnnouncement).toHaveBeenCalledWith('Teams balanced: 0.30 vs 0.20');
+        });
+    });
+
+    it('should balance team of no ratings', () => {
+        const ratings = [];
+        const playerList = [
+            {id: 1, name: 'A'},
+            {id: 2, name: 'B'}
+        ];
+        global.fetch = () => {
+            return Promise.resolve({
+                ok: true,
+                status: 200,
+                json: () => Promise.resolve(ratings)
+            });
+        };
+
+        roomSpy.getPlayerList.and.returnValue(playerList);
+        roomSpy.onCommand_balance().then(() => {
+            expect(roomSpy.setPlayerTeam).toHaveBeenCalledWith(1, 1);
+            expect(roomSpy.setPlayerTeam).toHaveBeenCalledWith(2, 2);
+            expect(roomSpy.sendAnnouncement).toHaveBeenCalledWith('Player ratings: A: 0.00, B: 0.00');
+            expect(roomSpy.sendAnnouncement).toHaveBeenCalledWith('Teams balanced: 0.00 vs 0.00');
+        });
+    });
+});
